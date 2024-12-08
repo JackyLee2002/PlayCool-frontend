bac01dbea68381103291918349840b55cd1c4f8c
chesterchan1119 <cc11197@gmail.com>
2024-12-08 15:56:56 +0800

    feat: refine Banner

--- playcool-frontend/pages/components/Banner/index.js
+++ playcool-frontend/pages/components/Banner/index.js
@@ -4,14 +4,16 @@ import './Banner.css';
 import bannerBg from '../../statics/BannerBG.png';
 import fourGuy from '../../statics/4guy.png';
 import concertTitle from '../../statics/ColdPlayConcertTitle.png';
+import yearTitle from '../../statics/2025.png';
 
 const Banner = () => {
     return (
         <div className="banner">
             <Image src={bannerBg} alt="Banner Background" layout="fill" objectFit="cover" />
             <div className="content">
-                <Image src={fourGuy} alt="4 Guys" className="fourGuy" />
+                <Image  src={fourGuy} alt="4 Guys" className="fourGuy" width={400} height={430} />
                 <Image src={concertTitle} alt="ColdPlay Concert Title" className="concertTitle" />
+                <Image src={yearTitle} alt="2025" className="yearTitle" />
             </div>
         </div>
     );
--- playcool-frontend/pages/components/NavBar/index.js
+++ playcool-frontend/pages/components/NavBar/index.js
@@ -10,15 +10,15 @@ import playCoolLogo from '../../statics/playCoolLogo.png';
 import Image from 'next/image';
 const NavBar = () => {
     return (
-        <AppBar  position="static" style={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
+        <AppBar  position="static" style={{ backgroundColor: 'transparent', boxShadow: 'none' , zIndex: "10", position: 'absolute'}}>
             <Toolbar className="toolbar">
                 <Image src={playCoolLogo} alt="PlayCool Logo" width={150} height={50} />
                 <div className="navItems">
                     <Link href="/events" passHref>
-                        <Button className="navButton">Vote Song List</Button>
+                        <Button className="navButton" sx={{'color' :'white'}}>Vote Song List</Button>
                     </Link>
                     <Link href="/my-tickets" passHref>
-                        <Button className="navButton">My Ticket</Button>
+                        <Button className="navButton" sx={{'color' :'white'}}>My Ticket</Button>
                     </Link>
                 </div>
                 <Button variant="outlined" startIcon={<PersonIcon />}>
