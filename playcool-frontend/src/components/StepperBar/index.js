7c6bb64319796e312e734f6df623e08c7a33402d
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-12-09 17:21:50 +0800

    feat:StepperBar

---
+++ playcool-frontend/src/components/StepperBar/index.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import styles from './StepperBar.module.css';
+
+const steps = ['Choose Time', 'Choose Seat', 'Checkout', 'Get Ticket'];
+
+const StepperBar = () => {
+    return (
+        <Box className={styles.stepperBar}>
+            <Stepper activeStep={1} alternativeLabel sx={{
+                '& .MuiStepIcon-root': {fontSize: '2rem'},
+                '& .MuiStepIcon-root.Mui-completed': {color: 'black', fontSize: '2rem'},
+                '& .MuiStepIcon-root.Mui-active': {color: 'black', fontSize: '2rem'}
+            }}>
+                {steps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel>{label}</StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+        </Box>
+    );
+};
+export default StepperBar;
