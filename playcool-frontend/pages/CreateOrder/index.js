86215d20da73f0f5733744973d31dc36a341b3bb
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-12-09 17:28:44 +0800

    refactor:rename

---
+++ playcool-frontend/pages/CreateOrder/index.js
@@ -0,0 +1,14 @@
+import NavBar from "@/src/components/NavBar";
+import StepperBar from "@/src/components/StepperBar";
+import Footer from "@/src/components/Footer";
+
+const CreateOrder = () => {
+    return (
+        <div>
+            <NavBar/>
+            <StepperBar/>
+            <Footer/>
+        </div>
+    );
+}
+export default CreateOrder;
