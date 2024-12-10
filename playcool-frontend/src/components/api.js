ca38e35b7a283d4175dab62f921a0f07497caaf0
Jacky Lee <leeja12@oocl.com>
2024-12-10 15:11:54 +0800

    feat: add my-orders page

--- playcool-frontend/src/components/api.js
+++ playcool-frontend/src/components/api.js
@@ -1,56 +1,91 @@
 export const fetchConcerts = async () => {
-    try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/concerts`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        if (!response.ok) {
-            throw new Error('Failed to fetch concerts');
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error fetching concerts:', error);
-        return { status: 'error', data: [] };
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/concerts`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch concerts");
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching concerts:", error);
+    return { status: "error", data: [] };
+  }
 };
 
-export const fetchConcert = async (id) => {
-    try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/concerts/${id}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        if (!response.ok) {
-            throw new Error('Failed to fetch concerts');
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error fetching concerts:', error);
-        return { status: 'error', data: [] };
+export const fetchConcert = async () => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/concerts/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch concerts");
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching concerts:", error);
+    return { status: "error", data: [] };
+  }
 };
 
 export const fetchAvailableSeats = async (id) => {
-    try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/areas/availableSeats/${id}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        if (!response.ok) {
-            throw new Error('Failed to fetch concerts');
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error fetching concerts:', error);
-        return { status: 'error', data: [] };
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/areas/availableSeats/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch concerts");
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching concerts:", error);
+    return { status: "error", data: [] };
+  }
+};
+
+export const fetchOrders = async (token) => {
+  if (!token) {
+    return { status: "error", data: [] };
+  }
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/order`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch orders");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+    return { status: "error", data: [] };
+  }
 };
