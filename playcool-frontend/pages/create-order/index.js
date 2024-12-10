f7d78dc5d71c25228c3bff83a8726083bcf7ff50
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-12-10 11:02:10 +0800

    refactor:remove unused code & reformat

--- playcool-frontend/pages/create-order/index.js
+++ playcool-frontend/pages/create-order/index.js
@@ -1,38 +1,15 @@
 import StepperBar from "@/src/components/StepperBar";
-import Footer from "@/src/components/Footer";
 import ConfirmOrder from "@/src/components/ConfirmOrder";
 import OrderDetail from "@/src/components/OrderDetail";
-import {useContext, useState} from "react";
-import {AuthContext} from "@/src/context/AuthContext";
 
 const CreateOrder = () => {
-    const [open, setOpen] = useState(false);
-    const { loginOpen, openLogin } = useContext(AuthContext);
 
-    const handleOpen = () => {
-        setOpen(true);
-        openLogin();
-    };
-    const handleClose = () => setOpen(false);
-
-
-    return (
-        <div>
+    return (<div>
             <StepperBar/>
             <OrderDetail/>
             <ConfirmOrder/>
-        </div>
-    );
+        </div>);
 }
 
-const modalStyle = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 700,
-    boxShadow: 24,
-    p: 4,
-};
 
 export default CreateOrder;
--- playcool-frontend/src/components/AccountMenu/index.js
+++ playcool-frontend/src/components/AccountMenu/index.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useContext} from 'react';
 import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import Menu from '@mui/material/Menu';
@@ -6,13 +7,9 @@ import MenuItem from '@mui/material/MenuItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
-import PersonAdd from '@mui/icons-material/PersonAdd';
-import Settings from '@mui/icons-material/Settings';
 import Logout from '@mui/icons-material/Logout';
 import {AuthContext} from "@/src/context/AuthContext";
-import {useContext} from "react";
 import {ShoppingCart} from "@mui/icons-material";
 
 export default function AccountMenu() {
