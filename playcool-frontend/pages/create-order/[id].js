31067d7638b71e7d5d422e11f56db2eff6f5deb7
Jacky Lee <leeja12@oocl.com>
2024-12-11 00:29:28 +0800

    fix: use Promise all

--- playcool-frontend/pages/create-order/[id].js
+++ playcool-frontend/pages/create-order/[id].js
@@ -1,50 +1,85 @@
 import StepperBar from "@/src/components/StepperBar";
 import ConfirmOrder from "@/src/components/ConfirmOrder";
 import OrderDetail from "@/src/components/OrderDetail";
-import {useEffect, useState} from "react";
-import {useRouter} from "next/router";
-import {fetchAreaById, fetchConcert} from "@/src/components/api";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { fetchAreaById, fetchConcert } from "@/src/components/api";
 
 const CreateOrder = () => {
-    const route = useRouter();
-    const router = useRouter();
-    const [selectedAreaId, setSelectedAreaId] = useState(1);
-    const [concertId, setConcertId] = useState(0);
-    const order = {
-        concerName: "",
-        concertDate: null,
-        venueName: "",
-        areaName: "",
-        price: "",
-        concertImage: "https://www.coldplay.com/wp/wp-content/uploads/2024/10/FM.webp"
+  const route = useRouter();
+  const router = useRouter();
+  const [selectedAreaId, setSelectedAreaId] = useState(1);
+  const [concertId, setConcertId] = useState(0);
+  const [order, setOrder] = useState({
+    concerName: "",
+    concertDate: null,
+    venueName: "",
+    areaName: "",
+    price: "",
+    concertImage:
+      "https://www.coldplay.com/wp/wp-content/uploads/2024/10/FM.webp",
+  });
+  //   let order = {
+  //     concerName: "",
+  //     concertDate: null,
+  //     venueName: "",
+  //     areaName: "",
+  //     price: "",
+  //     concertImage:
+  //       "https://www.coldplay.com/wp/wp-content/uploads/2024/10/FM.webp",
+  //   };
+  useEffect(() => {
+    if (!route.query.id || !router.query.selectedAreaId) {
+      return;
     }
-    useEffect(() => {
-        if (!route.query.id) {
-            return;
-        }
-        if (!router.query.selectedAreaId) {
-            return;
-        }
-        setConcertId(route.query.id);
-        setSelectedAreaId(router.query.selectedAreaId);
-        if (concertId) {
-            fetchConcert(concertId).then((data) => {
-                order.concerName = data.title;
-                order.concertDate = data.dateTime;
-            })
-        }
-        if (selectedAreaId) {
-            fetchAreaById(selectedAreaId).then((data) => {
-                order.venueName = data.venue.name;
-                order.areaName = data.name;
-                order.price = data.price;
-            }, [router]);
-        }
-}, [router, concertId, selectedAreaId]);
-    return (<div>
-        <StepperBar index={2}/>
-        <OrderDetail props={order}/>
-        <ConfirmOrder props={concertId}/>
-    </div>);}
+
+    let newOrder = {
+      concerName: "",
+      concertDate: null,
+      venueName: "",
+      areaName: "",
+      price: "",
+      concertImage:
+        "https://www.coldplay.com/wp/wp-content/uploads/2024/10/FM.webp",
+    };
+
+    console.log(route.query.id);
+    console.log(router.query.selectedAreaId);
+
+    const fetchConcertData = fetchConcert(route.query.id).then((data) => {
+      newOrder.concerName = data.title;
+      newOrder.concertDate = data.dateTime;
+      newOrder.a = 1;
+      console.log("Done first");
+    });
+
+    const fetchAreaData = fetchAreaById(router.query.selectedAreaId).then(
+      (data) => {
+        newOrder.venueName = data.venue.name;
+        newOrder.areaName = data.name;
+        newOrder.price = data.price;
+        newOrder.b = 2;
+        console.log("Done second");
+      }
+    );
+
+    Promise.all([fetchConcertData, fetchAreaData]).then(() => {
+      console.table(newOrder);
+      setOrder(newOrder);
+    });
+  }, [router.query.selectedAreaId, route.query.id]);
+
+  useEffect(() => {
+    console.table(order);
+  }, [order]);
+
+  return (
+    <div>
+      <StepperBar index={2} />
+      <OrderDetail props={order} concertId={router.query.id} />
+      <ConfirmOrder props={route.query.id} />
+    </div>
+  );
+};
 
 export default CreateOrder;
