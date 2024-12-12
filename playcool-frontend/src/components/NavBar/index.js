7c6b369b998e95f36944f8736487ad920ac3cba0
Ryan Liang <ryan.liang@oocl.com>
2024-12-12 22:37:24 +0800

    fix: logo width

--- playcool-frontend/src/components/NavBar/index.js
+++ playcool-frontend/src/components/NavBar/index.js
@@ -20,9 +20,9 @@ const NavBar = ({handleOpen}) => {
             height: '50px',
         }}>
             <Toolbar className={styles.toolbar} style={{padding: '0px'}}>
-                <Link href="/" passHref style={{flexGrow: 1}}>
+                <Link href="/" passHref style={{flexGrow: 1, display: 'flex', alignItems: 'center'}}>
                     <Image src={playCoolLogo} alt="PlayCool Logo"
-                           style={{ margin: "10px", minHeight: 20, width: 'auto', maxWidth: '100%'}}/>
+                           style={{ margin: "10px", width: '100px', height: 'auto'}}/>
                 </Link>
                 <div className={styles.navItems}>
                     <Link href="/song-list" passHref>
