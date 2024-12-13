535460be95d40c8e9282bb588843af46a29ab630
Jacky Lee <leeja12@oocl.com>
2024-12-13 12:39:06 +0800

    fix: text on Home Page

--- playcool-frontend/src/components/OtherSouvenirItem/index.js
+++ playcool-frontend/src/components/OtherSouvenirItem/index.js
@@ -1,28 +1,37 @@
-import React, { Suspense } from 'react';
-import { IconButton } from '@mui/material';
-import styles from './OtherSouvenirItem.module.css';
+import React, { Suspense } from "react";
+import { IconButton } from "@mui/material";
+import styles from "./OtherSouvenirItem.module.css";
 
-const OtherSouvenirItem = ({ imagePath,site }) => {
-    const handleBuyClick = () => {
-        window.location.href = site;
-    };
+const OtherSouvenirItem = ({ imagePath, site }) => {
+  const handleBuyClick = () => {
+    window.location.href = site;
+  };
 
-    return (
-        <div className={styles.OtherSouvenirItem}>
-            <img
-                src="/drag-right-white.gif"
-                alt="Drag Right"
-                className={styles.dragIcon}
-            />
-            <div className={styles.OtherSouvenirCanvas} style={{ width: '600px', height: '300px' }} >
-                <img src={imagePath} alt="Souvenir" style={{ maxWidth: '80%', height: '80%' }} />
-            </div>
-            <div className={styles.OtherSouvenirDescription}>
-                <h3 className={styles.OtherSouvenirNav} onClick={handleBuyClick}>Click to check it out !</h3>
-                <p>Perfect souvenir for fans to be worn by Coldplay during their concerts.</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.OtherSouvenirItem}>
+      <img
+        src="/drag-right-white.gif"
+        alt="Drag Right"
+        className={styles.dragIcon}
+      />
+      <div
+        className={styles.OtherSouvenirCanvas}
+        style={{ width: "600px", height: "300px" }}
+      >
+        <img
+          src={imagePath}
+          alt="Souvenir"
+          style={{ maxWidth: "80%", height: "80%" }}
+        />
+      </div>
+      <div className={styles.OtherSouvenirDescription}>
+        <h3 className={styles.OtherSouvenirNav} onClick={handleBuyClick}>
+          Click to check it out !
+        </h3>
+        <p>Perfect souvenir for fans from the concerts.</p>
+      </div>
+    </div>
+  );
 };
 
 export default OtherSouvenirItem;
--- playcool-frontend/src/components/SouvenirItem/index.js
+++ playcool-frontend/src/components/SouvenirItem/index.js
@@ -1,42 +1,52 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { useGLTF, OrbitControls } from '@react-three/drei';
-import { IconButton } from '@mui/material';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import styles from './SouvenirItem.module.css';
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, OrbitControls } from "@react-three/drei";
+import { IconButton } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import styles from "./SouvenirItem.module.css";
 
 const Model = ({ gltfPath }) => {
-    const { scene } = useGLTF(gltfPath);
-    return <primitive object={scene} />;
+  const { scene } = useGLTF(gltfPath);
+  return <primitive object={scene} />;
 };
 const SouvenirItem = ({ gltfPath, site }) => {
-    const handleBuyClick = () => {
-        window.location.href = site;
-    };
+  const handleBuyClick = () => {
+    window.location.href = site;
+  };
 
-    return (
-        <div className={styles.SouvenirItem}>
-            <img
-                src="/drag-right-white.gif"
-                alt="Drag Right"
-                className={styles.dragIcon}
-            />
-            <div className={styles.SouvenirCanvas} style={{ width: '600px', height: '400px' }}>
-                <Canvas camera={{ position: [0, 0, 1.4] }}>
-                    <ambientLight intensity={0.5} />
-                    <directionalLight position={[10, 10, 5]} intensity={1} />
-                    <Suspense fallback={null}>
-                        <Model gltfPath={gltfPath} />
-                    </Suspense>
-                    <OrbitControls enablePan={false} enableZoom={true} autoRotate={true} autoRotateSpeed={10} />
-                </Canvas>
-            </div>
-            <div className={styles.SouvenirDescription}>
-                <h3 className={styles.SouvenirNav} onClick={handleBuyClick}>Click to check it out!</h3>
-                <p>Perfect souvenir for fans to be worn.</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.SouvenirItem}>
+      <img
+        src="/drag-right-white.gif"
+        alt="Drag Right"
+        className={styles.dragIcon}
+      />
+      <div
+        className={styles.SouvenirCanvas}
+        style={{ width: "600px", height: "400px" }}
+      >
+        <Canvas camera={{ position: [0, 0, 1.4] }}>
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[10, 10, 5]} intensity={1} />
+          <Suspense fallback={null}>
+            <Model gltfPath={gltfPath} />
+          </Suspense>
+          <OrbitControls
+            enablePan={false}
+            enableZoom={true}
+            autoRotate={true}
+            autoRotateSpeed={10}
+          />
+        </Canvas>
+      </div>
+      <div className={styles.SouvenirDescription}>
+        <h3 className={styles.SouvenirNav} onClick={handleBuyClick}>
+          Click to check it out!
+        </h3>
+        <p>Perfect souvenir for fans from the concerts.</p>
+      </div>
+    </div>
+  );
 };
 
 export default SouvenirItem;
