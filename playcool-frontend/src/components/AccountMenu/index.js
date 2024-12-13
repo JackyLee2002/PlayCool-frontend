dd75deda5200eb602acb045d240cd674c7103cc6
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-12-13 08:43:41 +0800

    refactor:move username in accountMenu

--- playcool-frontend/src/components/AccountMenu/index.js
+++ playcool-frontend/src/components/AccountMenu/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useContext } from "react";
+import {useContext} from "react";
 import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
 import Menu from "@mui/material/Menu";
@@ -9,109 +9,102 @@ import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import Logout from "@mui/icons-material/Logout";
-import { AuthContext } from "@/src/context/AuthContext";
-import { ShoppingCart } from "@mui/icons-material";
+import {AuthContext} from "@/src/context/AuthContext";
+import {ShoppingCart} from "@mui/icons-material";
 import Link from "next/link";
 
 export default function AccountMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const { user, logout } = useContext(AuthContext);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  return (
-    <>
-      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
-        <Tooltip title="Account settings">
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            sx={{ ml: 2,
-          mr:6
-          }}
-            aria-controls={open ? "account-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-          >
-            <Avatar sx={{ width: 32, height: 32 }}>
-              {user.name.slice(0, 1)}
-            </Avatar>
-          </IconButton>
-        </Tooltip>
-      </Box>
-      <Menu
-        anchorEl={anchorEl}
-        id="account-menu"
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        slotProps={{
-          paper: {
-            elevation: 0,
-            sx: {
-              overflow: "visible",
-              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-              mt: 1.5,
-              "& .MuiAvatar-root": {
-                width: 32,
-                height: 32,
-                ml: -0.5,
-                mr: 1,
-              },
-              "&::before": {
-                content: '""',
-                display: "block",
-                position: "absolute",
-                top: 0,
-                right: 14,
-                width: 10,
-                height: 10,
-                bgcolor: "background.paper",
-                transform: "translateY(-50%) rotate(45deg)",
-                zIndex: 0,
-              },
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: "right", vertical: "top" }}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-      >
-        <MenuItem onClick={handleClose}>
-          <IconButton
-            sx={{ height: 24, width: 24, mr: 1.2, ml: 0.5 }}
-            size="small"
-            aria-controls={open ? "account-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-          >
-            <Avatar>{user.name.slice(0, 1)}</Avatar>
-          </IconButton>{" "}
-          Profile
-        </MenuItem>
-        <Link href="/my-orders">
-          <MenuItem onClick={handleClose}>
-            <ListItemIcon>
-              <ShoppingCart fontSize="small" />
-            </ListItemIcon>
-            My orders
-          </MenuItem>
-        </Link>
-        <Divider />
-
-        <MenuItem disabled={true} sx={{ color: "#000000" }}>
-          {user.name}
-        </MenuItem>
-        <MenuItem onClick={logout}>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
-      </Menu>
-    </>
-  );
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const {user, logout} = useContext(AuthContext);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    return (
+        <>
+            <Box sx={{display: "flex", alignItems: "center", textAlign: "center"}}>
+                <Tooltip title="Account settings">
+                    <IconButton
+                        onClick={handleClick}
+                        size="small"
+                        sx={{
+                            ml: 2,
+                            mr: 6
+                        }}
+                        aria-controls={open ? "account-menu" : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? "true" : undefined}
+                    >
+                        <Avatar sx={{width: 32, height: 32}}>
+                            {user.name.slice(0, 1)}
+                        </Avatar>
+                    </IconButton>
+                </Tooltip>
+            </Box>
+            <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                slotProps={{
+                    paper: {
+                        elevation: 0,
+                        sx: {
+                            overflow: "visible",
+                            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                            mt: 1.5,
+                            "& .MuiAvatar-root": {
+                                width: 32,
+                                height: 32,
+                                ml: -0.5,
+                                mr: 1,
+                            },
+                            "&::before": {
+                                content: '""',
+                                display: "block",
+                                position: "absolute",
+                                top: 0,
+                                right: 14,
+                                width: 10,
+                                height: 10,
+                                bgcolor: "background.paper",
+                                transform: "translateY(-50%) rotate(45deg)",
+                                zIndex: 0,
+                            },
+                        },
+                    },
+                }}
+                transformOrigin={{horizontal: "right", vertical: "top"}}
+                anchorOrigin={{horizontal: "right", vertical: "bottom"}}
+            >
+                <MenuItem>
+                    <IconButton
+                        sx={{height: 24, width: 24, mr: 1.2, ml: 0.5}}
+                        size="small"
+                    >
+                        <Avatar>{user.name.slice(0, 1)}</Avatar>
+                    </IconButton>{" "}
+                    {user.name}
+                </MenuItem>
+                <Link href="/my-orders">
+                    <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                            <ShoppingCart fontSize="small"/>
+                        </ListItemIcon>
+                        My orders
+                    </MenuItem>
+                </Link>
+                <Divider/>
+                <MenuItem onClick={logout}>
+                    <ListItemIcon>
+                        <Logout fontSize="small"/>
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
+        </>
+    );
 }
